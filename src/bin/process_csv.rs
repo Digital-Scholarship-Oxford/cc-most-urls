806d623585a4fe28226f68acdaade23882994bf4
Pierre Marshall <pierre.marshall@gmail.com>
2025-12-28 21:34:18 +0000

    feat: calculate quantiles in process_csv.rs

--- src/bin/process_csv.rs
+++ src/bin/process_csv.rs
@@ -1,4 +1,5 @@
 use polars::prelude::*;
+
 fn main() {
     let csv_schema = Schema::from_iter(vec![
         Field::new("raw_characters".into(), DataType::UInt16),
@@ -48,43 +49,70 @@ fn main() {
     // println!("{grouped_df}");
     // }
 
-    for status_code in [1u8, 2u8, 3u8, 4u8, 5u8] {
-        let mean_average = lazy_frame
-            .clone()
-            .filter(col("status_code").eq(status_code))
-            .mean()
-            .collect()
-            .unwrap();
-        println!("mean average for {status_code}xx");
-        println!("{mean_average}");
-    }
+    // for status_code in [1u8, 2u8, 3u8, 4u8, 5u8] {
+    //     let mean_average = lazy_frame
+    //         .clone()
+    //         .filter(col("status_code").eq(status_code))
+    //         .mean()
+    //         .collect()
+    //         .unwrap();
+    //     println!("mean average for {status_code}xx");
+    //     println!("{mean_average}");
+    // }
 
-    let total_mean_average = lazy_frame
+    // let total_mean_average = lazy_frame
+    //     .clone()
+    //     .select([col("raw_characters"), col("graphemes")])
+    //     .mean()
+    //     .collect()
+    //     .unwrap();
+    // println!("total mean average is");
+    // println!("{total_mean_average}");
+
+    // for status_code in [1u8, 2u8, 3u8, 4u8, 5u8] {
+    //     let median_average = lazy_frame
+    //         .clone()
+    //         .filter(col("status_code").eq(status_code))
+    //         .median()
+    //         .collect()
+    //         .unwrap();
+    //     println!("median average for {status_code}xx");
+    //     println!("{median_average}");
+    // }
+
+    // let total_median_average = lazy_frame
+    //     .clone()
+    //     .select([col("raw_characters"), col("graphemes")])
+    //     .median()
+    //     .collect()
+    //     .unwrap();
+    // println!("total median average is");
+    // println!("{total_median_average}");
+
+    let quantile = lazy_frame
         .clone()
         .select([col("raw_characters"), col("graphemes")])
-        .mean()
+        .quantile(lit(0.90), polars::prelude::QuantileMethod::Nearest)
         .collect()
         .unwrap();
-    println!("total mean average is");
-    println!("{total_mean_average}");
+    println!("90th quantile is");
+    println!("{quantile}");
 
-    for status_code in [1u8, 2u8, 3u8, 4u8, 5u8] {
-        let median_average = lazy_frame
-            .clone()
-            .filter(col("status_code").eq(status_code))
-            .median()
-            .collect()
-            .unwrap();
-        println!("median average for {status_code}xx");
-        println!("{median_average}");
-    }
-
-    let total_median_average = lazy_frame
+    let quantile = lazy_frame
         .clone()
         .select([col("raw_characters"), col("graphemes")])
-        .median()
+        .quantile(lit(0.80), polars::prelude::QuantileMethod::Nearest)
         .collect()
         .unwrap();
-    println!("total median average is");
-    println!("{total_median_average}");
+    println!("80th quantile is");
+    println!("{quantile}");
+
+    let quantile = lazy_frame
+        .clone()
+        .select([col("raw_characters"), col("graphemes")])
+        .quantile(lit(0.75), polars::prelude::QuantileMethod::Nearest)
+        .collect()
+        .unwrap();
+    println!("75th quantile is");
+    println!("{quantile}");
 }
